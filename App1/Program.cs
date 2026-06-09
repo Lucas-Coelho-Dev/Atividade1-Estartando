9faf6538fce43b21ede6789152aa9bc2acc4c5e0
Lucas_Coelho_Dev <lucasgcoelho@gmail.com>
2026-06-08 22:51:47 -0300

    Subindo projetos

--- App1/Program.cs
+++ App1/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 //Programa 1
+Console.WriteLine("Programa 1");
 var nome = NomeCompleto();
 Console.WriteLine(nome);
 
@@ -13,6 +14,7 @@ string NomeCompleto()
 
 
 //Programa 2 
+Console.WriteLine("Programa 2");
 var idade = idadePessoa();
 Console.WriteLine(idade);
 
@@ -24,5 +26,30 @@ short idadePessoa()
 
 
 //Programa 3
+Console.WriteLine("Programa 3");
+decimal preco1 = 20.90m;
+decimal preco2 = 70m;
+
+var soma = preco1 + preco2;
+Console.WriteLine(soma);
+Console.WriteLine("Preço = " + preco1);
+
+
+//Programa 4
+Console.WriteLine("Programa 4");
+const string nomeFazenda = " Rancho Verde";
+Console.WriteLine("Fazenda" + nomeFazenda);
+
+//Programa 5
+Console.WriteLine("Programa 5");
+
+int numero1 = 10;
+int numero2 = 5;
+
+int multiplicacao = numero1 * numero2;
+
+Console.WriteLine("Resultado da multiplicação: " + multiplicacao);
+
+
 
 
